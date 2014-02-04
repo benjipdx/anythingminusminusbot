6ed5d7fccc9dbcc3a062ae2748480b4badf1bf0c
Benjamin Reichert <breicher@cat.pdx.edu>
2014-02-03 18:02:52 -0800

    calcportal--, added for loop to allow calportal---

--- bot.bash
+++ bot.bash
@@ -8,20 +8,28 @@ fi
 
 tail -f .botfile | openssl s_client -connect irc.cat.pdx.edu:6697 | while true; do
   if [[ -z $started ]] ; then
-    echo "USER d2lbot d2lbot d2lbot :d2lbot" >> .botfile
-    echo "NICK d2lminusminus" >> .botfile
+    echo "USER calcportal-- calcportal-- calcportal-- :calcportal--" >> .botfile
+    echo "NICK calcportal" >> .botfile
     echo "JOIN #$chan $key" >> .botfile
     started="yes"
   fi
   read irc
   echo $irc
-  if `echo $irc | cut -d ' ' -f 1 | grep PING > /dev/null`; then
+  if `echo $irc | cut -d ' ' -f 1 | grep PING > /dev/null` ; then
     echo "PONG" >> .botfile
-  elif `echo $irc | grep PRIVMSG | grep d2l > /dev/null` ; then
-    nick="${irc%%!*}"; nick="${nick#:}"
-    if [[ $nick != 'd2lminusminus' ]] ; then
+    elif `echo $irc | grep PRIVMSG | grep "calcportal---" > /dev/null` ; then
+   nick="${irc%%!*}"; nick="${nick#:}"
+   if [[ $nick != 'calcportal' ]] ; then
       chan=`echo $irc | cut -d ' ' -f 3`
-      echo "PRIVMSG $chan :d2l--" >> .botfile
+      for i in {1..10}; do
+       echo "PRIVMSG $chan :calcportal--" >> .botfile
+      done
+   fi
+  elif `echo $irc | grep PRIVMSG | grep calcportal > /dev/null` ; then
+    nick="${irc%%!*}"; nick="${nick#:}"
+    if [[ $nick != 'calcportal' ]] ; then
+      chan=`echo $irc | cut -d ' ' -f 3`
+      echo "PRIVMSG $chan :calcportal--" >> .botfile
     fi
   fi
 done
