2992dda0d3af4aa6fdfc05146fdd6b5447296312
Benjamin Reichert <breicher@cat.pdx.edu>
2014-02-03 18:32:21 -0800

    paramaterizing

--- bot.bash
+++ bot.bash
@@ -6,10 +6,16 @@ if [ $2 != '' ] && [ -f $2 ] ; then
   key=`cat $2`
 fi
 
+#botname
+ircnick=calcportal
+
+#thing you hate most in the world
+minusminus=calcportal
+
 tail -f .botfile | openssl s_client -connect irc.cat.pdx.edu:6697 | while true; do
   if [[ -z $started ]] ; then
-    echo "USER calcportal-- calcportal-- calcportal-- :calcportal--" >> .botfile
-    echo "NICK calcportal" >> .botfile
+    echo "USER $ircnick-- $ircnick-- $ircnick-- :$ircnick--" >> .botfile
+    echo "NICK $ircnick" >> .botfile
     echo "JOIN #$chan $key" >> .botfile
     started="yes"
   fi
@@ -17,19 +23,19 @@ tail -f .botfile | openssl s_client -connect irc.cat.pdx.edu:6697 | while true; 
   echo $irc
   if `echo $irc | cut -d ' ' -f 1 | grep PING > /dev/null` ; then
     echo "PONG" >> .botfile
-    elif `echo $irc | grep PRIVMSG | grep "calcportal---" > /dev/null` ; then
+    elif `echo $irc | grep PRIVMSG | grep "$minusminus---" > /dev/null` ; then
    nick="${irc%%!*}"; nick="${nick#:}"
-   if [[ $nick != 'calcportal' ]] ; then
+   if [[ $nick != $ircnick ]] ; then
       chan=`echo $irc | cut -d ' ' -f 3`
       for i in {1..10}; do
-       echo "PRIVMSG $chan :calcportal--" >> .botfile
+       echo "PRIVMSG $chan :$minusminus--" >> .botfile
       done
    fi
-  elif `echo $irc | grep PRIVMSG | grep calcportal > /dev/null` ; then
+  elif `echo $irc | grep PRIVMSG | grep $minusminus > /dev/null` ; then
     nick="${irc%%!*}"; nick="${nick#:}"
-    if [[ $nick != 'calcportal' ]] ; then
+    if [[ $nick != $ircnick ]] ; then
       chan=`echo $irc | cut -d ' ' -f 3`
-      echo "PRIVMSG $chan :calcportal--" >> .botfile
+      echo "PRIVMSG $chan :$minusminus--" >> .botfile
     fi
   fi
 done
